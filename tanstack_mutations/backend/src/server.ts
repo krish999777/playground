4b0a4ae29fccbbb6740a6f7025776e8fd6ade6c7
Krish Shah <krishcshah09@gmail.com>
2026-06-08 11:42:42 +0530

    basic express app done

--- tanstack_mutations/backend/src/server.ts
+++ tanstack_mutations/backend/src/server.ts
@@ -1,10 +1,24 @@
 import express from 'express'
+import type {Request,Response} from 'express'
 
 const app=express()
 
-app.get('/todos',(req,res)=>{
+app.use(express.json())
+app.get('/todos',(req:Request,res:Response)=>{
     res.status(200).json({
         id:1,
         title:"tanstack", 
+        description:"Learn useQuery from tanStack"
     })
 })
+app.post('/todos',(req:Request,res:Response)=>{
+    res.status(201).json({
+        id:2,
+        title:req.body.title,
+        description:req.body.description
+    })
+})
+app.delete('todo',(req:Request,res:Response)=>{
+    res.status(204).send()
+})
+app.listen(8000,()=>console.log('App listening on port 8000'))
