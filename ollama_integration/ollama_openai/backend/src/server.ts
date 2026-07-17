a2221672c8ed201d713e13e0d3b323a837b77e80
Krish Shah <krishcshah09@gmail.com>
2026-07-17 22:56:24 +0530

    added funtion calling functionality dynamic messages array and multi step workflow

--- ollama_integration/ollama_openai/backend/src/server.ts
+++ ollama_integration/ollama_openai/backend/src/server.ts
@@ -1,8 +1,8 @@
 import OpenAI from 'openai'
 import express from 'express'
 import dotenv from 'dotenv'
-import * as z from 'zod'
-import { zodResponseFormat } from "openai/helpers/zod"
+// import * as z from 'zod'
+// import { zodResponseFormat } from "openai/helpers/zod"
 
 dotenv.config()
 
@@ -13,40 +13,146 @@ const openai=new OpenAI({
     baseURL:'http://localhost:11434/v1'
 })
 
-const resumeSchema=z.object({
-    skills:z.array(z.string()),
-    education:z.array(z.string()),
-    experience:z.array(z.string())
-})
+// const resumeSchema=z.object({
+//     skills:z.array(z.string()),
+//     education:z.array(z.string()),
+//     experience:z.array(z.string())
+// })
+
+function isSkillInDemand(skill:string){
+    console.log(skill)
+    return Math.random()>0.5
+}
+function calculateExperience(startDate:any,endDate:any){
+    console.log(startDate,endDate)
+    return 1
+}
+
+type AssistantToolCallType={
+    role:'assistant',
+    content:null,
+    tool_calls:any
+}
+
+type MessageType={
+    role:'system'|'user'|'assistant',
+    content:string,
+}
+type MessageToolCallType={
+    role:'tool',
+    tool_call_id:string,
+    content:string
+}
+
+let messages:(MessageType|AssistantToolCallType|MessageToolCallType)[]=[
+        {
+            role:"system" as const,
+            content:`
+            You will be be asked if a skill is in demand or not and/or to calculate the users experience
+            You have two functions isSkillInDemand(skill) and calculateExperience(startDate,endDate)
+            Only call these functions if the user explicitly asks if a specific skill is in demand or if user asks to calculate experience
+            Do not call the function otherwise
+            You have to call there functions according to what to the user asks and display the result in this format:
+            - Yes {skill} is/is not in demand
+            - Your experience in {company} is (x)
+
+            You have to use skill in demand part for as many skills there are and same for experience
+            If one/both of them is missing then dont include that sentance in the response
+            `,
+        },
+        {
+            role:"user" as const,
+            content:`
+            My name is krish
+            `
+        }
+    ]
 
 const response=await openai.chat.completions.create({
     model:'lfm2.5:8b',
-    messages:[
-        {
-            role:"system",
-            content:`
-            You are a resume analyzer
-            you will recieve a professional summary of a person and you have to segregate it into the following in JSON:
-            1)skills:array of professional skills(string).
-            2)Experience:array of company name (string). Include only if it is a company the user has worked in or is working
-            3)Education: array of institute name (string). Include only if is an institude or school the user studied or studies in
-            `
+    messages,
+    tools:[
+  {
+    type: "function",
+    function: {
+      name: "isSkillInDemand",
+      description: "Check if skill is in demand",
+      parameters: {
+        type: "object",
+        properties: {
+            skill: {
+              type: "string",
+              description: "Skill to check if it is in demand",
+            },
         },
-        {
-            role:"user",
-            content:`
-            I am a full stack developer, specializing in the mern stack + typescript and sql. 
-            I study in Shri bhagubhai mafatlal polytechnic. 
-            I have built resumeHub project with prisma and cloudinary and websocket
-            `
-        }
-    ],
-    response_format:zodResponseFormat(resumeSchema,'resume')
+        required:['skill']
+      },
+      strict: true,
+    },
+  },
+  {
+    type: "function",
+    function: {
+      name: "calculateExperience",
+      description: "Calculate experience by passing start and end date",
+      parameters: {
+        type: "object",
+        properties: {
+            startDate: {
+              type: "string",
+              description: "Start date of job",
+            },
+            endDate:{
+              type: "string",
+              description: "End date of job",
+            }
+        },
+        required:['startDate','endDate']
+      },
+      strict: true,
+    },
+  },
+],
+})
+if(response.choices[0]?.message.content){
+    messages.push({
+        role:'assistant',
+        content:response.choices[0].message.content
+    })
+}else{
+    messages.push({
+        role:'assistant',
+        content:null,
+        tool_calls:response.choices[0]?.message.tool_calls
+    })
+}
+
+(response.choices[0]?.message.tool_calls||[]).forEach((tool:any)=>{
+    if(tool.function.name==='isSkillInDemand'){
+        const argument=JSON.parse(tool.function.arguments)
+        const isInDemand=isSkillInDemand(argument.skill)
+        messages.push({
+          role: "tool",
+          tool_call_id: tool.id,
+          content:String(isInDemand),
+        })
+    }else{
+        const argument=JSON.parse(tool.function.arguments)
+        const exp=calculateExperience(argument.startDate,argument.endDate)
+        messages.push({
+          role: "tool",
+          tool_call_id: tool.id,
+          content:String(exp),
+        })
+    }
 })
 
-const parsed=JSON.parse(response.choices[0]?.message.content||"{}")
-console.log(response.choices[0]?.message.content)
-console.log(parsed)
+const finalResponse=await openai.chat.completions.create({
+    model:'lfm2.5:8b',
+    messages
+})
+
+console.log(finalResponse.choices[0]?.message.content)
 
 const PORT=process.env.PORT||8000
 app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`))
