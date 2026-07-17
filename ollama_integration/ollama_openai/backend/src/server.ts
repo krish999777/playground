742047cae8ace8a14083e6bb2400645de38fd5b9
Krish Shah <krishcshah09@gmail.com>
2026-07-17 18:44:25 +0530

    added system prompt and response_format for strict json responses

--- ollama_integration/ollama_openai/backend/src/server.ts
+++ ollama_integration/ollama_openai/backend/src/server.ts
@@ -1,6 +1,8 @@
 import OpenAI from 'openai'
 import express from 'express'
 import dotenv from 'dotenv'
+import * as z from 'zod'
+import { zodResponseFormat } from "openai/helpers/zod"
 
 dotenv.config()
 
@@ -11,30 +13,40 @@ const openai=new OpenAI({
     baseURL:'http://localhost:11434/v1'
 })
 
-app.get('/',async (req,res)=>{
-    const {question}=req.query
-    if(!question){
-        return res.status(200).send("Add a query parameter question to get ai response (?question=...)")
-    }
-    const response=await openai.chat.completions.create({
+const resumeSchema=z.object({
+    skills:z.array(z.string()),
+    education:z.array(z.string()),
+    experience:z.array(z.string())
+})
+
+const response=await openai.chat.completions.create({
     model:'lfm2.5:8b',
     messages:[
         {
+            role:"system",
+            content:`
+            You are a resume analyzer
+            you will recieve a professional summary of a person and you have to segregate it into the following in JSON:
+            1)skills:array of professional skills(string).
+            2)Experience:array of company name (string). Include only if it is a company the user has worked in or is working
+            3)Education: array of institute name (string). Include only if is an institude or school the user studied or studies in
+            `
+        },
+        {
             role:"user",
-            content:String(question)
+            content:`
+            I am a full stack developer, specializing in the mern stack + typescript and sql. 
+            I study in Shri bhagubhai mafatlal polytechnic. 
+            I have built resumeHub project with prisma and cloudinary and websocket
+            `
         }
     ],
-    stream:true
+    response_format:zodResponseFormat(resumeSchema,'resume')
 })
-let message=""
-for await (const chunk of response){
-    message+=chunk.choices[0]?.delta.content
-    res.write(chunk.choices[0]?.delta.content??'Error')
-    console.log(chunk.choices[0]?.delta.content??'Error')
-}
 
-res.end()
-})
+const parsed=JSON.parse(response.choices[0]?.message.content||"{}")
+console.log(response.choices[0]?.message.content)
+console.log(parsed)
 
 const PORT=process.env.PORT||8000
 app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`))
