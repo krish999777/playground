ab0e0be903a5765b7d65b5b9b56b76a0d1bc2ccc
Krish Shah <krishcshah09@gmail.com>
2026-07-24 10:06:16 +0530

    phase 0 complete

--- langchain learning/src/server.ts
+++ langchain learning/src/server.ts
@@ -1,8 +1,20 @@
 import express from 'express'
+import { initChatModel,HumanMessage,SystemMessage,AIMessage } from "langchain"
 
 const app=express()
 
+const model=await initChatModel('lfm2.5:8b',{
+    modelProvider: "ollama"
+})
 
+const res=await model.invoke([
+    new SystemMessage('Respond in one line only'),
+    new HumanMessage('My name is krish. What is node.js'),
+    new AIMessage('Nice to meet you krish, Node.js is server side scripting language in js'),
+    new HumanMessage('What is my name?')
+])
+
+console.log(res.content)
 
 const PORT=8000
 app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`))
