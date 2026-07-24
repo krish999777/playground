f6f59b95d4a2c1e46b5f9a92680d3fb420bee186
Krish Shah <krishcshah09@gmail.com>
2026-07-24 22:36:51 +0530

    phase 2 complete

--- langchain learning/src/server.ts
+++ langchain learning/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { initChatModel } from "langchain"
-import { ChatPromptTemplate,MessagesPlaceholder } from "@langchain/core/prompts";
+import {JsonOutputParser} from '@langchain/core/output_parsers'
 
 const app=express()
 
@@ -8,26 +8,22 @@ const model=await initChatModel('lfm2.5:8b',{
     modelProvider: "ollama"
 })
 
-const prompt = ChatPromptTemplate.fromMessages([
-    ['system','Respond in {lines} line only'],
-    new MessagesPlaceholder('fewShot'),
-    ['human','What is {topic}']
+const parser=new JsonOutputParser()
+
+const res=await model.invoke([
+    {role:'system',content:`
+        Respond in JSON only
+        {
+            "name":"",
+            "age":0
+        }
+        `},
+    {role:'human',content:'Who is William Shakesphere'}
 ])
-
-const messages=await prompt.invoke({
-    lines:1,
-    topic:'E in that',
-    fewShot:[
-        ['human','What is MERN'],
-        ['ai','MongoDB Express React Node']
-    ]
-})
-
-console.log(messages)
-
-const res=await model.invoke(messages)
-
 console.log(res.content)
+const output=await parser.invoke(res)
+
+console.log(output)
 
 const PORT=8000
 app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`))
