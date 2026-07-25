e97ea2313df7bcdd62e50bdb502d2834fe6ebc0d
Krish Shah <krishcshah09@gmail.com>
2026-07-25 16:14:18 +0530

    chaining using pipe

--- langchain learning/src/server.ts
+++ langchain learning/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { initChatModel } from "langchain"
+import {ChatPromptTemplate} from '@langchain/core/prompts'
 import * as z from 'zod'
 
 const app=express()
@@ -9,16 +10,21 @@ const model=await initChatModel('lfm2.5:8b',{
 })
 
 const PersonSchema=z.object({
-    name:z.string().describe('The name of the person referred by the user'),
-    age:z.number().positive().describe('Age of the person referred by the user')
+    name:z.string().describe('Name of what was referred by the user'),
+    age:z.number().describe('age of what aws referred by the user')
 })
 
-const structuredModel = model.withStructuredOutput(PersonSchema)
-
-const res=await structuredModel.invoke([
-    {role:'system',content:'Only include information explicitly known. Do not guess. If a field is unknown, return null.'},
-    {role:'human',content:'Tell me about cupboards'}
+const chatTemplate=ChatPromptTemplate.fromMessages([
+    ['system','Only include information explicitly known. Do not guess. If a field is unknown, return null.'],
+    ['human','Tell me about {item}']
 ])
+
+const chain=chatTemplate.pipe(model.withStructuredOutput(PersonSchema))
+
+const res=await chain.invoke({
+    item:'william shakesphear'
+})
+
 console.log(res)
 
 const PORT=8000
