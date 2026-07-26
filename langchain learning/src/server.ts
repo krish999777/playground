bbab3d9b2b50488dbdf0ecc330401d85184c57bb
Krish Shah <krishcshah09@gmail.com>
2026-07-26 20:04:28 +0530

    document splitter implemented

--- langchain learning/src/server.ts
+++ langchain learning/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import {initChatModel} from "langchain"
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 const app=express()
 
@@ -11,7 +12,14 @@ const model=await initChatModel('lfm2.5:8b',{
 const loader=new PDFLoader('./iso27001.pdf')
 const docs=await loader.load()
 
-console.log(docs.length,docs[0],docs[0]?.pageContent,docs[0]?.metadata,docs[0]?.id)
+const splitter= new RecursiveCharacterTextSplitter({
+    chunkSize:500,
+    chunkOverlap:50,
+})
+
+const chunks = await splitter.splitDocuments(docs)
+
+console.log(chunks.length,chunks[0],chunks[1])
 
 const PORT=8000
 app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`))
