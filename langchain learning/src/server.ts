0a83fc988c18c4ff36ac1e2ee4318bdf55fa58b5
Krish Shah <krishcshah09@gmail.com>
2026-07-26 19:57:02 +0530

    pdf loader for rag

--- langchain learning/src/server.ts
+++ langchain learning/src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express'
-import {initChatModel,tool,createAgent} from "langchain"
-import {ChatPromptTemplate} from '@langchain/core/prompts'
-import * as z from 'zod'
+import {initChatModel} from "langchain"
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 
 const app=express()
 
@@ -9,58 +8,10 @@ const model=await initChatModel('lfm2.5:8b',{
     modelProvider: "ollama"
 })
 
-const chatTemplate=ChatPromptTemplate.fromMessages([
-    ['system','You are a helpful ai assistant to assist the user'],
-    ['human','{input}']
-])
+const loader=new PDFLoader('./iso27001.pdf')
+const docs=await loader.load()
 
-const calc=tool(({expression}:{expression:string})=>{
-    const ans=eval(expression)
-    return ans
-},{
-    name:'Calculator for arithmetic expressions',
-    description:'Calculator which takes arithmentic expression as arugments and returns the answer',
-    schema:z.object({
-        expression:z.string()
-    })
-})
-
-const currentTime=tool(()=>new Date(),{
-    name:'Get current time',
-    description:'Getting current time',
-    schema:z.object({})
-})
-
-const languageInfo=tool(({language})=>{
-    console.log(language)
-    return {
-        creator:'krish',
-        year:'2009'
-    }
-},{
-    name:'GetProgrammingLanguageInfo',
-    description:'Get info about programming language',
-    schema:z.object({
-        language:z.string()
-    })
-})
-
-const chat=await chatTemplate.invoke({
-    input:'What is 57*42? What is the time? Give me some info about Java'
-})
-
-// const result1=await calc.invoke({expression:'57*42'})
-// const result2=await currentTime.invoke({})
-// const result3=await languageInfo.invoke({language:'React'})
-
-const agent = createAgent({
-    model,
-    tools:[calc,currentTime,languageInfo]
-})
-
-const res=await agent.invoke(chat)
-
-console.log(res.messages[res.messages.length-1]?.content)
+console.log(docs.length,docs[0],docs[0]?.pageContent,docs[0]?.metadata,docs[0]?.id)
 
 const PORT=8000
 app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`))
