f2eb65093f044c1f7a5f3a42e746fbe78cc6dbda
Krish Shah <krishcshah09@gmail.com>
2026-07-29 17:03:55 +0530

    rollback to agent part and implemented memory saver and checkpoint with mock session id in agents

--- langchain learning/src/server.ts
+++ langchain learning/src/server.ts
@@ -1,67 +1,82 @@
 import express from 'express'
-import {initChatModel} from "langchain"
-// import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-// import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
-import {OllamaEmbeddings} from '@langchain/ollama'
-import { QdrantVectorStore } from "@langchain/qdrant"
-import {ChatPromptTemplate} from '@langchain/core/prompts'
-import dotenv from 'dotenv'
-import { RunnableLambda } from '@langchain/core/runnables';
-import type { Document } from 'langchain';
-import {StringOutputParser} from '@langchain/core/output_parsers'
-
+import {initChatModel,tool,createAgent} from "langchain"
+// import {ChatPromptTemplate} from '@langchain/core/prompts'
+import {MemorySaver} from '@langchain/langgraph'
+import * as z from 'zod'
 
 const app=express()
-dotenv.config()
+
+const checkpointer=new MemorySaver()
 
 const model=await initChatModel('lfm2.5:8b',{
     modelProvider: "ollama"
 })
 
-const embeddingModel=new OllamaEmbeddings({
-    model:'nomic-embed-text-v2-moe:latest',
+// const chatTemplate=ChatPromptTemplate.fromMessages([
+//     ['system','You are a helpful ai assistant to assist the user. You dont have to use the tools. Use the tools only if needed'],
+//     ['human','{input}']
+// ])
+
+const calc=tool(({expression}:{expression:string})=>{
+    const ans=eval(expression)
+    return ans
+},{
+    name:'Calculator for arithmetic expressions',
+    description:'Calculator which takes arithmentic expression as arugments and returns the answer',
+    schema:z.object({
+        expression:z.string()
+    })
 })
 
-// const loader=new PDFLoader('./iso27001.pdf')
-// const docs=await loader.load()
+const currentTime=tool(()=>new Date(),{
+    name:'Get current time',
+    description:'Getting current time',
+    schema:z.object({})
+})
 
-// const splitter= new RecursiveCharacterTextSplitter({
-//     chunkSize:500,
-//     chunkOverlap:50,
+const languageInfo=tool(({language})=>{
+    console.log(language)
+    return {
+        creator:'krish',
+        year:'2009'
+    }
+},{
+    name:'GetProgrammingLanguageInfo',
+    description:'Get info about programming language',
+    schema:z.object({
+        language:z.string()
+    })
+})
+
+// const chat1=await chatTemplate.invoke({
+//     input:'Hi my name is krish'
 // })
 
-// const chunks = await splitter.splitDocuments(docs)
+// const chat2=await chatTemplate.invoke({
+//     input:'What is my name?'
+// })
 
-const QUESTION='What is monitoring and measurement'
+// const result1=await calc.invoke({expression:'57*42'})
+// const result2=await currentTime.invoke({})
+// const result3=await languageInfo.invoke({language:'React'})
 
-const formatDocs=new RunnableLambda({
-    func:(docs:Document[])=>({context:docs.map(doc=>doc.pageContent).join('\n\n'),input:QUESTION})
+const agent = createAgent({
+    model,
+    tools:[calc,currentTime,languageInfo],
+    checkpointer
 })
 
-const parser=new StringOutputParser()
+const res=await agent.invoke({messages:[['human','Hi my name is krish']]},{configurable:{thread_id:'1'}})
+const res2=await agent.invoke({messages:[['human','What is my name?']]},{configurable:{thread_id:'1'}})
+const res3=await agent.invoke({messages:[['human','What is my name?']]},{configurable:{thread_id:'2'}})
 
-const vectorStore=await QdrantVectorStore.fromExistingCollection(embeddingModel,{url:process.env.QDRANT_URL!,collectionName: "iso27001",})
 
-const retriever=vectorStore.asRetriever({k:4})
-
-const promptTemplate=ChatPromptTemplate.fromMessages([
-    [
-        'system',`
-        You have to answer the users question only from the provided context. If the answer is not in the context, say you dont know.\n
-        context:{context}
-    `],
-    [
-        'human',`
-        {input}
-        `
-    ]
-])
-
-const chain=retriever.pipe(formatDocs).pipe(promptTemplate).pipe(model).pipe(parser)
-
-const res=await chain.invoke(QUESTION)
-
-console.log(res)
+console.log('res 1')
+console.log(res.messages[res.messages.length-1]?.content)
+console.log('res 2')
+console.log(res2.messages[res2.messages.length-1]?.content)
+console.log('res 3')
+console.log(res3.messages[res3.messages.length-1]?.content)
 
 const PORT=8000
 app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`))
