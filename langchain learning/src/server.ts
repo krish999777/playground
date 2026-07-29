bdffdc729f4e674e839fddccfd6d11a7f4c5f8cd
Krish Shah <krishcshah09@gmail.com>
2026-07-29 15:00:27 +0530

    created lcel chain for full rag flow

--- langchain learning/src/server.ts
+++ langchain learning/src/server.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 import {initChatModel} from "langchain"
-import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+// import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+// import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import {OllamaEmbeddings} from '@langchain/ollama'
 import { QdrantVectorStore } from "@langchain/qdrant"
+import {ChatPromptTemplate} from '@langchain/core/prompts'
 import dotenv from 'dotenv'
+import { RunnableLambda } from '@langchain/core/runnables';
+import type { Document } from 'langchain';
+import {StringOutputParser} from '@langchain/core/output_parsers'
 
 
 const app=express()
@@ -18,23 +22,46 @@ const embeddingModel=new OllamaEmbeddings({
     model:'nomic-embed-text-v2-moe:latest',
 })
 
-const loader=new PDFLoader('./iso27001.pdf')
-const docs=await loader.load()
+// const loader=new PDFLoader('./iso27001.pdf')
+// const docs=await loader.load()
 
-const splitter= new RecursiveCharacterTextSplitter({
-    chunkSize:500,
-    chunkOverlap:50,
+// const splitter= new RecursiveCharacterTextSplitter({
+//     chunkSize:500,
+//     chunkOverlap:50,
+// })
+
+// const chunks = await splitter.splitDocuments(docs)
+
+const QUESTION='What is monitoring and measurement'
+
+const formatDocs=new RunnableLambda({
+    func:(docs:Document[])=>({context:docs.map(doc=>doc.pageContent).join('\n\n'),input:QUESTION})
 })
 
-const chunks = await splitter.splitDocuments(docs)
+const parser=new StringOutputParser()
 
 const vectorStore=await QdrantVectorStore.fromExistingCollection(embeddingModel,{url:process.env.QDRANT_URL!,collectionName: "iso27001",})
 
 const retriever=vectorStore.asRetriever({k:4})
 
-const vectorDoc=await retriever.invoke('What is ISO 27001?')
+const promptTemplate=ChatPromptTemplate.fromMessages([
+    [
+        'system',`
+        You have to answer the users question only from the provided context. If the answer is not in the context, say you dont know.\n
+        context:{context}
+    `],
+    [
+        'human',`
+        {input}
+        `
+    ]
+])
 
-console.log(vectorDoc)
+const chain=retriever.pipe(formatDocs).pipe(promptTemplate).pipe(model).pipe(parser)
+
+const res=await chain.invoke(QUESTION)
+
+console.log(res)
 
 const PORT=8000
 app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`))
