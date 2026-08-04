4c093f94fc0a61286aa0117a50e20df989a0ca46
Krish Shah <krishcshah09@gmail.com>
2026-08-04 18:48:17 +0530

    static paralell execution

--- LangGraph learning/src/server.ts
+++ LangGraph learning/src/server.ts
@@ -1,43 +1,39 @@
 import express from 'express'
-import {START,END,StateGraph,Annotation,MemorySaver,messagesStateReducer} from '@langchain/langgraph'
-import {initChatModel,SystemMessage,HumanMessage} from 'langchain'
-import type {BaseMessage} from 'langchain'
+import {START,END,StateGraph,Annotation} from '@langchain/langgraph'
 
 const app=express()
 
-const model=await initChatModel('lfm2.5:8b',{
-    modelProvider:'ollama'
-})
-
-const checkpointer=new MemorySaver()
-
 const messagesAnnotation=Annotation.Root({
-    messages:Annotation<BaseMessage[]>({
-        reducer:messagesStateReducer,
-        default:()=>[new SystemMessage('You are helpful assistant and you have to respond in one line')]
-    }),
-    question:Annotation<string>()
+    logs:Annotation<string[]>({
+        default:()=>[],
+        reducer:(current,update)=>[...current,...update]
+    })
 })
 
 const graph=new StateGraph(messagesAnnotation)
-.addNode('getResponse',async (state)=>{
-    const humanMessage=new HumanMessage(state.question)
-    const res=await model.invoke([...state.messages,humanMessage])
-    return {messages:[humanMessage,res]}
+.addNode('A',()=>({logs:['A']}))
+.addNode('B',()=>{
+    console.log('B started')
+    return {logs:['B']}
 })
-.addEdge(START,'getResponse')
-.addConditionalEdges('getResponse',(state)=>(state.messages[state.messages.length-1]!).content.split(' ').length>20?'getResponse':END)
-
-const graphApp=graph.compile({checkpointer})
-
-const res=await graphApp.invoke({question:'My name is krish,What is the MERN stack'},{
-    configurable:{thread_id:'ABC'}
+.addNode('C',async ()=>{
+    await new Promise((resolve)=>{
+        setTimeout(()=>resolve(''),2000)
+    })
+    return {logs:['C']}
+    
 })
+.addEdge(START,'A')
+.addEdge('A','B')
+.addEdge('A','C')
+.addEdge('B',END)
+.addEdge('C',END)
 
-const res2=await graphApp.invoke({question:'What is my name'},{
-    configurable:{thread_id:'ABC'}
-})
-console.log(res2.messages,res2.question)
+const graphApp=graph.compile()
+
+const res=await graphApp.invoke({})
+
+console.log(res)
 
 const PORT=8000
 app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`))
