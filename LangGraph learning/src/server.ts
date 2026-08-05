2c43266145ff56483843337038a94159215d8d75
Krish Shah <krishcshah09@gmail.com>
2026-08-05 21:38:39 +0530

    dynamic paralell with set

--- LangGraph learning/src/server.ts
+++ LangGraph learning/src/server.ts
@@ -1,48 +1,33 @@
 import express from 'express'
-import {START,END,StateGraph,Annotation,Command} from '@langchain/langgraph'
+import {START,END,StateGraph,Annotation,Send,Command} from '@langchain/langgraph'
 
 const app=express()
 
 const messagesAnnotation=Annotation.Root({
-    logs:Annotation<string[]>({
-        default:()=>[],
+    topics:Annotation<string[]>({
+        default:():string[]=>[],
         reducer:(current,update)=>[...current,...update]
     }),
-    mathComplete:Annotation<boolean>(),
-    scienceComplete:Annotation<boolean>()
+    summary:Annotation<string[]>({
+        default:()=>[],
+        reducer:(current,update)=>[...current,...update]
+    })
 })
 
 const graph=new StateGraph(messagesAnnotation)
-.addNode('Math',()=>({logs:['Math']}))
-.addNode('Science',()=>({logs:['Science']}))
-.addNode('Supervisor',async (state)=>{
-    if(state.mathComplete&&state.scienceComplete){
-        return new Command({
-            goto:END
-        })
-    }
-    else if(state.mathComplete){
-        return new Command({
-            update:{scienceComplete:true},
-            goto:'Science'
-        })
-    }else{
-        return new Command({
-            update:{mathComplete:true},
-            goto:'Math'
-        })
-    }
-    
-},{
-    ends:['Science','Math',END]
+.addNode('planner',(state)=>(new Command({
+        goto:state.topics.map((topic:string)=>new Send('summerize',{topic}))
+    })
+),{
+    ends:['summerize']
 })
-.addEdge(START,'Supervisor')
-.addEdge('Science','Supervisor')
-.addEdge('Math','Supervisor')
+.addNode('summerize',(state:{topic:string})=>({summary:[`summary of ${state.topic}`]}))
+.addEdge(START,'planner')
+.addEdge('summerize',END)
 
 const graphApp=graph.compile()
 
-const res=await graphApp.invoke({})
+const res=await graphApp.invoke({topics:['hahah','lol','krish']})
 
 console.log(res)
 
