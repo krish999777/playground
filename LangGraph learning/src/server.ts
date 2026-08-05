756eb5fc8b056c9737821c94a2a3fb64cd5772ee
Krish Shah <krishcshah09@gmail.com>
2026-08-05 19:47:25 +0530

    iterative supervisor workflow

--- LangGraph learning/src/server.ts
+++ LangGraph learning/src/server.ts
@@ -8,26 +8,30 @@ const messagesAnnotation=Annotation.Root({
         default:()=>[],
         reducer:(current,update)=>[...current,...update]
     }),
-    math:Annotation<boolean>(),
-    supervisorState:Annotation<'preWorker'|'postWorker'>()
+    mathComplete:Annotation<boolean>(),
+    scienceComplete:Annotation<boolean>()
 })
 
 const graph=new StateGraph(messagesAnnotation)
 .addNode('Math',()=>({logs:['Math']}))
-.addNode('Science',()=>{
-    return {logs:['Science']}
-})
+.addNode('Science',()=>({logs:['Science']}))
 .addNode('Supervisor',async (state)=>{
-    if(state.supervisorState==='postWorker'){
+    if(state.mathComplete&&state.scienceComplete){
         return new Command({
             goto:END
         })
     }
-
-    return new Command({
-        update:{supervisorState:'postWorker'},
-        goto:state.math?'Math':'Science'
-    })
+    else if(state.mathComplete){
+        return new Command({
+            update:{scienceComplete:true},
+            goto:'Science'
+        })
+    }else{
+        return new Command({
+            update:{mathComplete:true},
+            goto:'Math'
+        })
+    }
     
 },{
     ends:['Science','Math',END]
@@ -38,7 +42,7 @@ const graph=new StateGraph(messagesAnnotation)
 
 const graphApp=graph.compile()
 
-const res=await graphApp.invoke({math:true})
+const res=await graphApp.invoke({})
 
 console.log(res)
 
